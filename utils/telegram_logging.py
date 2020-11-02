da3f2e8aec4cb40b13abcc4888cd80f2df2da26e
Artur Kasymov <art.kasymov@gmail.com>
2020-11-02 23:42:54 +0100

    disable once more

--- utils/telegram_logging.py
+++ utils/telegram_logging.py
@@ -1,15 +1,17 @@
 import requests
 
-import logging
-logging.getLogger("requests").setLevel(logging.CRITICAL)
-
 
 class TelegramLogger(object):
 
-    def __init__(self, bot_token: str, chat_id: str):
+    def __init__(self, bot_token: str, chat_id: str, disable_req_log: bool = True):
         self._api_url = f'https://api.telegram.org/bot{bot_token}/'
         self._message_url = self._api_url + 'sendMessage'
         self._chat_id = chat_id
+
+        if disable_req_log:
+            import logging
+            logging.getLogger("requests").setLevel(logging.CRITICAL)
+            logging.getLogger("urllib3").setLevel(logging.CRITICAL)
 
     def log(self, message: str):
         try:
