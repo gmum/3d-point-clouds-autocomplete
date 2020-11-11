09f85617bd86b0792c90eb382cf949f38c1b4abd
Artur Kasymov <art.kasymov@gmail.com>
2020-11-11 04:12:19 +0100

    send images to TG fix

--- utils/telegram_logging.py
+++ utils/telegram_logging.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -6,7 +8,7 @@ class TelegramLogger(object):
     def __init__(self, bot_token: str, chat_id: str, disable_req_log: bool = True):
         self._api_url = f'https://api.telegram.org/bot{bot_token}/'
         self._message_url = self._api_url + 'sendMessage'
-        self._image_url = self._api_url + 'sendPhoto'
+        self._image_url = self._api_url + 'sendMediaGroup'
         self._chat_id = chat_id
 
         if disable_req_log:
@@ -24,11 +26,19 @@ class TelegramLogger(object):
         except Exception:
             pass
 
-    def log_images(self, image_pathes):
+    def log_images(self, image_paths):
         try:
-            files = {'image' + str(i): open(image_path, 'rb') for i, image_path in enumerate(image_pathes)}
-            data = {'chat_id': self._chat_id}
-            requests.post(self._image_url, files=files, data=data)
-
+            send_data = {
+                'chat_id': self._chat_id,
+                'media': json.dumps([
+                    {
+                        'type': 'photo',
+                        'media': f'attach://image_{i}.png',
+                    } for i in range(len(image_paths))
+                ])
+            }
+            files = {f'image_{i}.png': open(image_path, 'rb') for i, image_path in enumerate(image_paths)}
+            r = requests.post(self._image_url, params=send_data, files=files)
+            print(r.content)
         except Exception:
             pass
